c1bc2009b4337a315aa268c3b3e60d562d30f6e0
Chris <chris@felixalacampagne.be>
2021-09-19 16:34:53 +0200

    Tries to fake response to the HEAD /portal/challenge request. Not accepted by the radio.

--- src/main/java/com/felixalacampagne/recivaportal/RecivaPortalRest.java
+++ src/main/java/com/felixalacampagne/recivaportal/RecivaPortalRest.java
@@ -1,6 +1,10 @@
 package com.felixalacampagne.recivaportal;
 
-import com.sun.istack.logging.Logger;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HEAD;
@@ -11,52 +15,76 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.ResponseBuilder;
 import jakarta.ws.rs.core.UriInfo;
 
 @Path("/")
 public class RecivaPortalRest
 {
-	final Logger log = Logger.getLogger(this.getClass());
+	final Logger log = LoggerFactory.getLogger(this.getClass());
    @GET
    @Path("/challenge")
    @Produces(MediaType.TEXT_PLAIN)
-   public String getChallenge(@QueryParam("serial") String serial) 
+   public Response getChallenge(@QueryParam("serial") String serial, @QueryParam("sp") String sp)
    {
-   	log.info("serial:" + serial);
-   	// HEAD ?serial=0000df34&sp=v257-a-865-a-476
-   	return "Return from getChallenge";
+   	log.info("getChallenge: serial:" + serial + " sp:" + sp);
+   	ResponseBuilder responseBuilder = makeChallengeResponse(false, serial);
+      return responseBuilder.build();
    }
    
    @HEAD
    @Path("/challenge")
    @Produces(MediaType.TEXT_PLAIN)
-   public String getHeadChallenge(@QueryParam("serial") String serial) 
+   public Response getHeadChallenge(@QueryParam("serial") String serial, @QueryParam("sp") String sp)
    {
-   	log.info("serial:" + serial);
-   	// HEAD ?serial=0000df34&sp=v257-a-865-a-476
-   	return "Return from getHeadChallenge";
+   	// HEAD response should only return the Headers which would be returned by the equivalent GET request.
+   	// A GET request should follow but haven't seen one from the radio yet...
+   	log.info("getHeadChallenge: serial:" + serial + " sp:" + sp);
+   	ResponseBuilder responseBuilder = makeChallengeResponse(true, serial);
+      return responseBuilder.build();
    }   
 
    @HEAD
    @Path("/{name}")
    @Produces(MediaType.TEXT_PLAIN)
-   public String getHeadAny(@PathParam("name") String path, @Context UriInfo ui) 
+   public Response getHeadAny(@PathParam("name") String path, @Context UriInfo ui) 
    {
    	log.info("getHeadAny:" + path);
    	doAny(path, ui);
-   	return "getHeadAny called: path:" + path;
+   	ResponseBuilder responseBuilder = Response.status(200);
+      responseBuilder.lastModified(new Date());
+      return responseBuilder.build();
    }
    
    @GET
    @Path("/{name}")
    @Produces(MediaType.TEXT_PLAIN)
-   public String getAny(@PathParam("name") String path, @Context UriInfo ui) 
+   public Response getAny(@PathParam("name") String path, @Context UriInfo ui) 
    {
    	log.info("getAny: path:" + path);
    	doAny(path, ui);
-   	return "getAny called: path:" + path;
+   	ResponseBuilder responseBuilder = Response.status(200);
+      responseBuilder.entity("getAny called: path:" + path);
+      responseBuilder.lastModified(new Date());
+      return responseBuilder.build();
+
    }
 
+   private ResponseBuilder makeChallengeResponse(boolean bHead, String entity)
+   {
+   	ResponseBuilder responseBuilder = Response.status(200);
+      responseBuilder.lastModified(new Date());
+      responseBuilder.header("Content-Length", "" + entity.toString().length());
+      responseBuilder.header("x-reciva-challenge-format", "sernum");
+      responseBuilder.header("Set-Cookie", "JSESSIONID=ABCD1234; Path=/portal");
+      if(!bHead)
+      {
+      	responseBuilder.entity(entity.toString());
+      }
+      return responseBuilder;
+   }
+   
 	private void doAny(String path, UriInfo ui)
 	{
 		log.info("doAny: path:" + path);
