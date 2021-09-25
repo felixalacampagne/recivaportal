e3d5ae64ac60667c98a209fc8eca65f64d473429
chris <chris@chunky>
2021-09-25 10:59:29 +0200

    Still only getting the HEAD request...

--- src/main/java/com/felixalacampagne/recivaportal/RecivaPortalRest.java
+++ src/main/java/com/felixalacampagne/recivaportal/RecivaPortalRest.java
@@ -2,22 +2,26 @@ package com.felixalacampagne.recivaportal;
 
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HEAD;
+import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.ResponseBuilder;
 import jakarta.ws.rs.core.UriInfo;
+
 
 @Path("/")
 public class RecivaPortalRest
@@ -29,20 +33,28 @@ public class RecivaPortalRest
    public Response getChallenge(@QueryParam("serial") String serial, @QueryParam("sp") String sp)
    {
    	log.info("getChallenge: serial:" + serial + " sp:" + sp);
-   	ResponseBuilder responseBuilder = makeChallengeResponse(false, serial);
-      return responseBuilder.build();
+      return makeChallengeResponse(false, serial);
    }
    
    @HEAD
    @Path("/challenge")
    @Produces(MediaType.APPLICATION_OCTET_STREAM)
-   public Response getHeadChallenge(@QueryParam("serial") String serial, @QueryParam("sp") String sp)
+   public Response getHeadChallenge(@QueryParam("serial") String serial, 
+         @QueryParam("sp") String sp,
+         @Context HttpHeaders headers)
    {
    	// HEAD response should only return the Headers which would be returned by the equivalent GET request.
    	// A GET request should follow but haven't seen one from the radio yet...
    	log.info("getHeadChallenge: serial:" + serial + " sp:" + sp);
-   	ResponseBuilder responseBuilder = makeChallengeResponse(true, serial);
-      return responseBuilder.build();
+   	
+   	
+      MultivaluedMap<String, String> rh = headers.getRequestHeaders();
+      String str = rh.entrySet()
+                     .stream()
+                     .map(e -> e.getKey() + " = " + e.getValue())
+                     .collect(Collectors.joining("\n"));   	
+   	log.debug("getHeadChallenge: headers:\n" + str); // once again toString is forking useless! headers.toString());
+      return makeChallengeResponse(true, serial);
    }   
 
    @HEAD
@@ -71,19 +83,25 @@ public class RecivaPortalRest
 
    }
 
-   private ResponseBuilder makeChallengeResponse(boolean bHead, String entity)
+   private Response makeChallengeResponse(boolean bHead, String entity)
    {
+      String challenge = "1122334455667788";
+      Date lm = new Date(108,1,1,12,0); // 1 Jan 2008 12:00
    	ResponseBuilder responseBuilder = Response.status(200);
-      responseBuilder.lastModified(new Date());
-      responseBuilder.header("Content-Length", "" + entity.getBytes().length);
+   	Response response;
+      responseBuilder.lastModified(lm);
+      responseBuilder.header("Content-Length", "" + challenge.getBytes().length);
       responseBuilder.type(MediaType.APPLICATION_OCTET_STREAM);
       responseBuilder.header("X-Reciva-Challenge-Format", "sernum");
       responseBuilder.header("Set-Cookie", "JSESSIONID=ABCD1234; Path=/portal");
       if(!bHead)
       {
-      	responseBuilder.entity(entity.getBytes());
+      	responseBuilder.entity(challenge.getBytes());
       }
-      return responseBuilder;
+      response = responseBuilder.build();
+      // TODO log the header - response.toString() does not do this
+//      log.debug("makeChallengeResponse: Response:\n" + response.toString());
+      return response;
    }
    
 	private void doAny(String path, UriInfo ui)
